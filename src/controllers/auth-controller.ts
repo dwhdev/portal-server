e475213723755cd479c54c55c3ca7bb6137b3e60
José Carlos Palma <palmahn@gmail.com>
2020-11-15 22:25:35 -0600

    Implementación del endpoint para cierre de sesión.

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -39,6 +39,10 @@ export const login = async (req: Request, res: Response) => {
     }
 };
 
+export const logout = (req: Request, res: Response) => {
+    res.clearCookie('SESSIONID').status(200).json({ ok: true });
+};
+
 /**
  * Renueva el token para mantener la autenticación vigente.
  * @param req Información de la petición.
--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
-import { login, renewToken, verifyToken } from '../controllers/auth-controller';
+import { login, logout, renewToken, verifyToken } from '../controllers/auth-controller';
 import { checkAuthenticateJWT } from '../middlewares/jwt-checker';
 
 const router = Router();
 
 router.post('/login', login);
+
+router.post('/logout', logout);
 
 router.get('/check',
     [
