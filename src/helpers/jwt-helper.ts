b78c0ea990118d60d628bd36b77be32ebf85f700
José Carlos Palma <palmahn@gmail.com>
2021-09-29 11:39:05 -0600

    fix(helpers): Identificación de tipos de excepciones en 'generateJWT'

--- src/helpers/jwt-helper.ts
+++ src/helpers/jwt-helper.ts
@@ -1,6 +1,6 @@
-import { sign, verify } from 'jsonwebtoken';
+import { sign, verify, JsonWebTokenError, TokenExpiredError } from 'jsonwebtoken';
 
-const SECRET = process.env.JWT_SECRET;
+const SECRET: string = process.env.JWT_SECRET || '';
 
 interface IToken {
     iat: number;
@@ -12,14 +12,22 @@ interface IToken {
  * Genera un JWT para la autenticación de la aplicación.
  * @param username nombre de usuario.
  */
-export const generateJWT = (username: string): { token?: string | null, expiresIn?: number, message?: string; } => {
+export const generateJWT = (
+    username: string
+): { token?: string | null; expiresIn?: number; message?: string } => {
     try {
         const token = sign({}, SECRET, { expiresIn: '1h', subject: username });
         const { exp } = verify(token, SECRET) as IToken;
         return { token, expiresIn: exp };
-    } catch (ex) {
+    } catch (ex: any) {
         console.warn('Error al generar token:\n', ex);
-        return { message: ex.message };
+        switch (ex.constructor) {
+            case JsonWebTokenError:
+            case TokenExpiredError:
+                return { message: ex.message };
+            default:
+                return { message: 'Internal error.' };
+        }
     }
 };
 
@@ -34,11 +42,11 @@ export const verifyToken = (token: string) => {
             return { tokenError: { ok: false, message: 'Token invalido.' } };
         }
         return { authUser, expiresIn };
-    } catch (ex) {
-        switch (ex.name) {
-            case 'JsonWebTokenError':
+    } catch (ex: any) {
+        switch (ex.constructor) {
+            case JsonWebTokenError:
                 return { tokenError: { ok: false, message: 'Token invalido.' } };
-            case 'TokenExpiredError':
+            case TokenExpiredError:
                 return { tokenError: { ok: false, message: 'Token expirado.' } };
         }
         return { tokenError: { ok: false, message: ex.message } };
